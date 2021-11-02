ab8cf100304281e9d3da6c23e9e9e13af8a1a5e3
abhishekmittal15 <55945158+abhishekmittal15@users.noreply.github.com>
2021-11-02 09:02:37 +0530

    Added the cosine kernel

---
+++ cosine_similarity/1_normal_kernel/host/host.cpp
@@ -0,0 +1,82 @@
+#include "xcl2.hpp"
+#include <iostream>
+#include<vector>
+
+using std::cout;
+using std::endl;
+
+int main(int argc,char **argv){
+
+    if(argc!=3){
+        cout << "insufficient arguments" << endl;
+        return EXIT_FAILURE;
+    }
+
+    std::string kernel_name = "vmult";
+    unsigned int num_elements = std::stoi(argv[1]);
+    unsigned int size_bytes_input = num_elements * sizeof(uint32_t);
+    unsigned int size_bytes_output = num_elements * sizeof(uint64_t);
+    char *filename = argv[2];
+    auto binaryFile = xcl::read_binary_file(filename);
+    cl::Program::Binaries bins{{binaryFile.data(),binaryFile.size()}};
+
+    std::vector<cl::Device> devices=xcl::get_xil_devices();
+    cl::Device = devices[0];
+    cl_int err;
+    cl::Context context(device, NULL, NULL, NULL, &err);
+    cl::CommandQueue q(context, device, CL_QUEUE_PROFILING_ENABLE, &err);
+    cl::Program program(context, {device}, bins, &err);
+    cl::Kernel kernel(program, kernel_name, &err);
+
+    std::vector<uint32_t, aligned_allocator<uint32>> a(num_elements);
+    std::vector<uint32_t, aligned_allocator<uint32>> b(num_elements);
+    std::vector<uint64_t, aligned_allocator<uint64>> sw(num_elements);
+    std::vector<uint64_t, aligned_allocator<uint64>> hw(num_elements);
+
+    for (unsigned int i = 0;i<num_elements;i++){
+        a[i] = std::rand();
+        b[i] = std::rand();
+        sw[i] = a[i] * b[i];
+        hw[i] = 0;
+    }
+
+    cl::Buffer a_buf(context, static_cast<cl_mem_flags>(CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY), size_bytes_input, a.data(), &err);
+    cl::Buffer b_buf(context, static_cast<cl_mem_flags>(CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY), size_bytes_input, b.data(), &err);
+    cl::Buffer hw_buf(context, static_cast<cl_mem_flags>(CL_MEM_USE_HOST_PTR | CL_MEM_WRITE_ONLY), size_bytes_output, hw.data(), &err);
+
+    cl::Event event;
+    uint64_t kernel_start, kernel_end, kernel_queue, kernel_submit;
+
+    kernel.setArg(0, hw_buf);
+    kernel.setArg(1, a_buf);
+    kernel.setArg(2, b_buf);
+    kernel.setArg(3, num_elements);
+
+    q.enqueueMemMigrateObjects({a_buf, b_buf}, 0);
+
+    q.enqueueTask(kernel, nullptr, &event);
+    q.finish();
+    event.getProfilingInfo<uint64_t>(CL_PROFILING_COMMAND_START, &kernel_start);
+    event.getProfilingInfo<uint64_t>(CL_PROFILING_COMMAND_END, &kernel_end);
+    event.getProfilingInfo<uint64_t>(CL_PROFILING_COMMAND_QUEUED, &kernel_queue);
+    event.getProfilingInfo<uint64_t>(CL_PROFILING_COMMAND_SUBMIT, &kernel_submit);
+
+    auto duration = kernel_end - kernel_start;
+    cout<<kernel_queue<<endl;
+    cout << kernel << submit << endl;
+    cout << kernel_start << endl;
+    cout << kernel_end << endl;
+
+    q.enqueueMigrateMemObjects({hw_buf}, CL_MIGRATE_MEM_OBJECT_HOST);
+    q.finish();
+
+    for (unsigned int i = 0; i < num_elements;i++){
+        cout << a[i] << " + " << b[i] << " = " << sw[i] << " | " << hw[i] << endl;
+        if(sw[i]!=hw[i]){
+            cout << "TESTS FAILED" << endl;
+            return 1;
+        }
+    }
+
+    return 0;
+}
---
+++ cosine_similarity/1_normal_kernel/kernel/kernel.cpp
@@ -0,0 +1,16 @@
+extern "C"{
+    void vumlt(
+        uint64_t *out,
+        const uint32_t *a,
+        const uint32_t *b,
+        unsigned int num_elements     
+    ){
+    
+    vadd_loop:
+        for (int x = 0; x < num_elements; ++x)
+        {
+#pragma HLS LOOP_TRIPCOUNT min = num_elements max = num_elements
+            out[x] = a[x] * b[x];
+        }
+    }
+}
