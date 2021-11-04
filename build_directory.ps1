daee6ba32af53c6fd44d074cc93f2d57391a15dd
abhishekmittal15 <55945158+abhishekmittal15@users.noreply.github.com>
2021-11-05 03:44:45 +0530

    hmm

--- build_directory.ps1
+++ build_directory.ps1
@@ -1,18 +1,19 @@
 $var = Read-Host "Enter the name of the directory"
+$root = "D:\github Projects\FPGA_go_brrrr"
 mkdir $var
 $current_directory = Get-Location 
 Set-Location $current_directory/$var
 
 # Create the Makefiles and the xrt.ini and config.ini files 
-Copy-Item ..\..\common\Makefile .
-Copy-Item ..\..\common\design.cfg
-Copy-Item ..\..\common\xrt.ini
+Copy-Item $root\common\Makefile .
+Copy-Item $root\common\design.cfg
+Copy-Item $root\common\xrt.ini
 
 # Creating the host folder 
 mkdir host 
 Set-Location host 
-New-Item host.cpp 
-Copy-Item ..\..\common\*.cpp .
+Copy-Item $root\cos_sim\host.cpp .
+Copy-Item $root\common\*.cpp .
 Set-Location ..
 
 # Creating the kernel folder 
@@ -27,5 +28,5 @@ mkdir build
 # Create the includes folder 
 mkdir include
 Set-Location include 
-Copy-Item ../../common/*.hpp . 
+Copy-Item $root/common/*.hpp . 
 Set-Location ..
